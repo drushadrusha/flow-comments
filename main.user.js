242d7a262259f18b6357e41e181d15a29f5037f8
drushadrusha <drusha@drusha.me>
2023-03-06 19:45:24 +0700

    work

--- main.user.js
+++ main.user.js
@@ -13,11 +13,13 @@
     if (window.location.hostname == ("the-flow.ru")){
         var articleDiv = document.getElementsByClassName('article__text c_post_text');
         var div = document.createElement('div');
-        // make div 100% width
-        div.style.width = '100%';
-        // make div 100% height
-        div.style.height = '200px';
-        div.innerHTML = '<h3>comments</h3><div id="disqus_thread"></div><script type="text/javascript" src="https://the-flow2014.disqus.com/count.js"></script>';
+
+        var d = document, s = d.createElement('script');
+        s.src = 'https://the-flow2014.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+
+        div.innerHTML = '<div id="disqus_thread"></div>';
         articleDiv[0].appendChild(div);
         // remove last style tag on page
         var style = document.getElementsByTagName('style');
