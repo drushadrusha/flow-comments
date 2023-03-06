da031f1d125e5bcbef003b80465b272e68f663ff
drushadrusha <drusha@drusha.me>
2023-03-06 20:07:45 +0700

    readme & license

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# flow-comments
+## Возвращаем комментарии на The Flow.
+
+Userscript который возвращает комментарии на сайт the-flow.ru в том числе и старые.
+
+### Установка
+1. Установите расширение [Tampermonkey](https://www.tampermonkey.net/) для вашего браузера.
+2. Перейдите по [ссылке](https://github.com/drushadrusha/flow-comments/raw/master/main.user.js) и нажмите Install.
--- main.user.js
+++ main.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Enable Disqus on The-Flow.ru
+// @name         The Flow Comments
 // @namespace    https://github.com/drushadrusha
-// @version      1.1
-// @description  ...
+// @version      1.0
+// @description  Bring back comments on the-flow.ru
 // @author       drushadrusha
 // @match        https://the-flow.ru/*
 // @grant        none
@@ -13,12 +13,12 @@
     if (window.location.hostname == ("the-flow.ru")){
         var articleDiv = document.getElementsByClassName('article__text c_post_text');
         var div = document.createElement('div');
-
+        // load disqus
         var d = document, s = d.createElement('script');
         s.src = 'https://the-flow2014.disqus.com/embed.js';
         s.setAttribute('data-timestamp', +new Date());
         (d.head || d.body).appendChild(s);
-
+        // create comments div
         div.innerHTML = '<div id="disqus_thread"></div>';
         articleDiv[0].appendChild(div);
         // remove last style tag on page
